ec06318e8225ae78275bae639edf8a7924da40d1
ungung470 <h098s02617@chunchon.gwe.hs.kr>
2025-11-14 15:27:58 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
      labels[0]: {
        "texts": ["올림피아 보디빌더", "보디빌더", "남미 보디빌더"],
        "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
-       "videos": ["https://youtu.be/XXXXXXXXXXX"]
+       "videos": ["https://www.youtube.com/watch?v=Q6FtGzt1hxM"]
      },
 }
 
